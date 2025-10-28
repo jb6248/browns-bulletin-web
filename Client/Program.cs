86d0b548fc0b5dfe70e23161c2397c8f5f00a9a3
Kilian Jakstis <kilianjj@MacBookPro.lan>
2025-10-27 23:24:59 -0400

    added basic user/msg state

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using MudBlazor.Services;
+using BlazorApp.Client.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+builder.Services.AddScoped<UserState>();
+builder.Services.AddScoped<MessageState>();
 
 await builder.Build().RunAsync();
